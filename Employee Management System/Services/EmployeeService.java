b205b46ade9bba08021805d190f9a87c80ec8382
PrevinBogopa <previnbogopa@icloud.com>
2023-09-11 03:39:48 +0200

    Springboot create classes

---
+++ Employee Management System/Services/EmployeeService.java
@@ -0,0 +1,27 @@
+package com.zaio.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zaio.Entity.Employee;
+import com.zaio.Repositories.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+//    create save method to save employee
+	
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    // Create a method to save an employee
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+}
+	
+
